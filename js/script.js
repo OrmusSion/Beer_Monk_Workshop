b7e0ceeb433f56a915414d2c694417afb1ec0ed9
OrmusSion <ormussion@gmail.com>
2022-09-26 17:53:25 +0300

    azaza

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("Hop Monk " + "Beer");
+
+function test() {
+	console.log ("test 1");
+	console.log ("test 2");
+}
+
+test();
+
+let userName = "20";
+let userPass = "183461";
+
+console.log (userName);
+
+userName = userPass;
+
+console.log (userName);
